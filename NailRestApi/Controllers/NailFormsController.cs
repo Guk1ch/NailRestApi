fe7d0c8e1a454108b6ca1477e49e7b98dae58b22
Егор Артемьев <batman_batman_1910@mail.ru>
2023-05-27 17:07:41 +0300

    Nail form работает

---
+++ NailRestApi/Controllers/NailFormsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NailRestApi.Models;
+
+namespace NailRestApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public partial class NailFormsController : Controller
+    {
+        private readonly NailAppContext _context;
+
+        public NailFormsController(NailAppContext context)
+        {
+            _context = context;
+        }
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<NailForm>>> GetNailForms()
+		{
+			return await _context.NailForms.Include(x => x.Nails).ToListAsync();
+		}
+		[HttpGet("{id}")]
+		public ActionResult<NailForm> GetNailForm(int id)
+		{
+			var nailForm = _context.NailForms.Include(x => x.Nails).ToList().FirstOrDefault(x => x.Id == id);
+			if (nailForm == null)
+			{
+				return NotFound();
+			}
+			return nailForm;
+		}
+		[HttpPost]
+		public async Task<ActionResult<NailForm>> PostClient( NailForm nailForm)
+		{
+			//👉👈
+			_context.NailForms.Add(nailForm);
+			await _context.SaveChangesAsync();
+			return CreatedAtAction("GetNailForm", new { id = nailForm.Id }, nailForm);
+		}
+
+	}
+}
