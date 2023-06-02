018d276529c371249edfb89a043065b69604dedf
Егор Артемьев <batman_batman_1910@mail.ru>
2023-06-02 14:55:18 +0300

    Record edit

--- NailRestApi/Models/Record.cs
+++ NailRestApi/Models/Record.cs
@@ -3,7 +3,7 @@
 	public class Record
 	{
 		public int Id { get; set; }
-		public int IdClient { get; set; }
+		public int? IdClient { get; set; }
 		public string Time { get; set; }
         public int Cost { get; set; }
         public bool IsDone { get; set; }
